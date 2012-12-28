483a1abf5a19ffe98da58d38a8aec09a0bf0b827
Timo Sulg <timgluz@gmail.com>
2012-12-28 17:20:03 +0100

    added minimal search and info functionality

--- lib/veye/resource.rb
+++ lib/veye/resource.rb
@@ -2,14 +2,20 @@ module Veye
 	module JSONRequest
 		require 'json'
 
-		def getJSON(path)
+		def getJSON(path, params = nil, configs = nil)
+      request_params = {}
+      request_configs = {:content_type  => :json,
+                         :accept        => :json}
+
+      request_params.merge!(params) unless params.nil?
+      request_configs.merge!(configs) unless configs.nil?
+      request_configs[:params] = request_params unless request_params.empty?
 			response = nil
 			begin
-			 response = @resource[path].get :content_type  => "json", 
-			 	 							:accept 		=> "json"
+			 response = @resource[path].get request_configs
 			 response = JSON.parse(response)
 			rescue => e
-			  error(e.response)
+			   $stderr.puts e.response
 			end
 			response
 		end
@@ -18,19 +24,22 @@ module Veye
 	module API
   	  require 'rest_client'
 
-  	  CONFIGS = {:path => "https://www.versioneye.com/api/v1"}
-  	  class Resource 
+  	  CONFIGS = {:url => "https://www.versioneye.com/api/v1"}
+  	  class Resource
   		include JSONRequest
     	attr_reader :resource
-    	def initialize   
-      	  @resource = RestClient::Resource.new(CONFIGS[:path])
+    	def initialize(path = nil)
+          @full_path = CONFIGS[:url]
+          @full_path = "#{@full_path}#{path}" unless path.nil?
+
+      	  @resource = RestClient::Resource.new(@full_path)
     	end
   	  end
 
   	  class PrivateResource
     	attr_reader :resource
     	def initialize(username, password)
-      	  @resource = RestClient::Resource.new(CONFIGS[:path], username, password)
+      	  @resource = RestClient::Resource.new(CONFIGS[:url], username, password)
     	end
   	  end
 	end
