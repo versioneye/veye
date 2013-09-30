5c10799b468f42f223383af64a2b90168a2ca162
Timo Sulg <timgluz@gmail.com>
2013-09-30 18:45:04 +0200

    Added notifications when settings or api-key are missing.

--- lib/veye.rb
+++ lib/veye.rb
@@ -9,10 +9,72 @@ require 'veye/user.rb'
 require 'veye/pagination.rb'
 
 $global_options ||= nil 
+DEFAULT_CONFIG_FILE = ".veye.rc"
+DEFAULT_CONFIG_PATH = "~"
+
+def get_config_fullpath
+  File.expand_path("#{DEFAULT_CONFIG_PATH}/#{DEFAULT_CONFIG_FILE}")
+end
+
+def config_exists?
+  filepath = get_config_fullpath
+  File.exists?(filepath)
+end
+
+def self.check_configs(global_opts)
+  unless config_exists?
+    msg = sprintf("%s: %s\n",
+                  "config file doesnt exist. ".foreground(:red),
+                  "Use `veye initconfig` to initialize settings file.")
+    exit_now!(msg)
+  end
+  check_api_key(global_opts)
+
+  true
+end
+
 def init_environment
     #sets up required variables and modules to work on IRB or unittest
-    config_file = "#{ENV["HOME"]}/.veye.rc"
+    config_file = get_config_fullpath   
     $global_options = YAML.load_file(config_file)
-    $global_options[:url] = Veye::API::Resource.build_url($global_options)
-    
+    $global[:config_file] = config_file 
+    $global_options[:url] = Veye::API::Resource.build_url($global_options)  
+end
+
+def check_api_key(global_opts)
+  result = false
+  if global_opts[:api_key].nil? or global_opts[:api_key].match("add your api key")
+    msg = sprintf("%s: %s\n",
+                "Warning: API key is missing.".foreground(:yellow), 
+                "You cant access private data.")
+     print msg
+  else
+    result = true
+  end
+
+  result
+end
+
+def update_api_key
+  user_key = gets.chomp.to_s.strip
+  if user_key =~ /No/i
+    msg = sprintf("%s: %s",
+                  "Warning:".foreground(:yellow),
+                  " missing, you cant access private data.")
+    print msg
+  else
+    $global_options[:api_key] = user_key
+    save_configs
+  end
+end
+
+def save_configs
+  filepath = get_config_fullpath
+  File.open(filepath, 'w') do |f|
+    f.puts $global_options.to_yaml
+  end
+  msg = sprintf("%s: %s",
+         "Success".foreground(:green),
+         "new settings are saved into file: `#{filepath}`")
+  print msg
 end
--- lib/veye/package/search.rb
+++ lib/veye/package/search.rb
@@ -15,20 +15,21 @@ module Veye
         }
 
         def self.search(search_term, language = nil, group_id = nil, page = nil)
-            search_api = Veye::API::Resource.new "#{RESOURCE_PATH}/search"
-            search_response = nil
-            search_params = {:q => search_term.to_s}
+          search_api = Veye::API::Resource.new "#{RESOURCE_PATH}/search" 
+          
+          search_response = nil
+          search_params = {:q => search_term.to_s}
 
-            search_params[:lang] = Package.encode_language(language) unless language.nil?
-            search_params[:g] = group_id unless group_id.nil?
-            search_params[:page] = page unless page.nil?
+          search_params[:lang] = Package.encode_language(language) unless language.nil?
+          search_params[:g] = group_id unless group_id.nil?
+          search_params[:page] = page unless page.nil?
 
-            request_params = {:params => search_params}
-            search_api.resource["/#{search_term}"].get(request_params) do |response, request, result, &block|
-                search_response = API::JSONResponse.new(request, result, response)    
-            end
+          request_params = {:params => search_params}
+          search_api.resource["/#{search_term}"].get(request_params) do |response, request, result, &block|
+              search_response = API::JSONResponse.new(request, result, response)    
+          end
 
-            return search_response
+          return search_response
         end
 
         def self.format(search_results, format = 'pretty', paging = nil)
