18e37c4e914b70c869e8000d4912a42f4e070478
Timo Sulg <timgluz@gmail.com>
2015-08-09 11:28:02 +0200

    refactored github info cmd;

--- lib/veye/github/info.rb
+++ lib/veye/github/info.rb
@@ -3,6 +3,22 @@ require_relative '../base_executor.rb'
 
 module Veye
   module Github
+    module API
+      def self.encode_repo_key(repo_key)
+        repo_key.to_s.gsub(/\//, ":").gsub(/\./, "~")
+      end
+ 
+      def self.get_repo(api_key, repo_name)
+        safe_repo_name = self.encode_repo_key(repo_name)
+        github_api = Veye::API::Resource.new("#{RESOURCE_PATH}/#{safe_repo_name}")
+        qparams = {params: {api_key: api_key}}
+
+        github_api.resource.get(qparams) do |response, request, result|
+          Veye::API::JSONResponse.new(request, result, response)
+        end
+      end
+    end
+
     class Info < BaseExecutor
       @@output_formats = {
         'csv'     => Github::InfoCSV.new,
@@ -12,18 +28,10 @@ module Veye
       }
 
       def self.get_repo(api_key, repo_name, options)
-        github_api = API::Resource.new(RESOURCE_PATH)
-        results = nil
-        qparams = {params: {api_key: api_key}}
-        safe_repo_name = self.encode_repo_key(repo_name)
-
-        github_api.resource["/#{safe_repo_name}"].get(qparams) do |response, request, result|
-          results = API::JSONResponse.new(request, result, response)
-        end
+        results = API.get_repo(api_key, repo_name)
         catch_request_error(results, "Can not find repository `#{repo_name}`")
         show_results @@output_formats, results.data, options
       end
-
     end
   end
 end
---
+++ test/github_info_test.rb
@@ -0,0 +1,88 @@
+require 'test_helper'
+require 'csv'
+
+class GithubInfoTest < Minitest::Test
+  def setup
+    init_environment
+    @api_key = 'ba7d93beb5de7820764e'
+    @repo_name = 'versioneye/veye'
+  end
+
+  def test_api_call
+    VCR.use_cassette('github_info') do
+      res = Veye::Github::API.get_repo(@api_key, @repo_name)
+      refute nil, res
+      assert_equal 200, res.code
+      repo = res.data["repo"]
+      assert_equal "versioneye/veye", repo["fullname"]
+      assert_equal "ruby", repo["language"]
+      assert_equal "versioneye", repo["owner_login"]
+      assert_equal false, repo["private"]
+      assert_equal false, repo["fork"]
+      assert_equal [], res.data["imported_projects"]
+    end
+  end
+
+  def test_get_repo_default
+    VCR.use_cassette('github_info') do
+      output = capture_stdout do
+        Veye::Github::Info.get_repo(@api_key, @repo_name, {})
+      end
+
+      refute nil, output
+      rows = output.split(/\n/)
+      assert_equal "\t\e[32mversioneye/veye\e[0m - \e[1mruby\e[0m", rows[0]
+      assert_equal "\tDescription    : VersionEye command line tool ", rows[1]
+      assert_equal "\tOwner login    : versioneye", rows[2]
+      assert_equal "\tOwner type     : organization", rows[3]
+      assert_equal "\tPrivate        : false", rows[4]
+      assert_equal "\tFork           : false", rows[5]
+      assert_equal "\tBranches       : master, references", rows[6]
+    end
+  end
+
+  def test_get_repo_csv
+    VCR.use_cassette('github_info') do
+      output = capture_stdout do
+        Veye::Github::Info.get_repo(@api_key, @repo_name, {format: 'csv'})
+      end
+
+      refute nil, output
+      rows = CSV.parse(output)
+      assert_equal ["name", "language", "owner_login", "owner_type", "private", "fork", "branches", "imported_projects", " description"], rows[0]
+      assert_equal ["versioneye/veye", "ruby", "versioneye", "organization", "false", "false", "master|references", nil, "VersionEye command line tool "], rows[1]
+    end
+  end
+
+  def test_get_repo_json
+    VCR.use_cassette('github_info') do
+      output = capture_stdout do
+        Veye::Github::Info.get_repo(@api_key, @repo_name, {format: 'json'})
+      end
+
+      refute nil, output
+      dt = JSON.parse(output)
+      repo = dt["repo"]
+      assert_equal "versioneye/veye", repo["fullname"]
+      assert_equal "ruby", repo["language"]
+      assert_equal "versioneye", repo["owner_login"]
+      assert_equal "organization", repo["owner_type"]
+      assert_equal false, repo["private"]
+      assert_equal false, repo["fork"]
+      assert_equal ["master", "references"], repo["branches"]
+    end
+  end
+
+  def test_get_repo_table
+    VCR.use_cassette('github_info') do
+      output = capture_stdout do
+        Veye::Github::Info.get_repo(@api_key, @repo_name, {format: 'table'})
+      end
+
+      refute nil, output
+      rows = output.split(/\n/)
+      assert_equal "| name            | language | owner_login | owner_type   | private | fork  | branches   | imported_projects | description                   |", rows[3]
+      assert_equal "| versioneye/veye | ruby     | versioneye  | organization | false   | false | master     |                   | VersionEye command line tool  |", rows[5]
+    end
+  end
+end
