47c748baa25bb7baf60597191ff25625286eecda
Timo Sulg <timgluz@gmail.com>
2015-08-18 20:25:28 +0200

    rubocopped github/list class;

--- lib/veye/github/list.rb
+++ lib/veye/github/list.rb
@@ -3,9 +3,9 @@ require_relative '../base_executor.rb'
 
 module Veye
   module Github
+    # List class includes methods to see importable Github repositories
     class List < BaseExecutor
-
-      @@output_formats = {
+      @output_formats = {
         'csv'     => Github::ListCSV.new,
         'json'    => Github::ListJSON.new,
         'pretty'  => Github::ListPretty.new,
@@ -13,14 +13,15 @@ module Veye
       }
 
       def self.get_list(api_key, options)
-        results = Veye::API::Github.get_list(api_key, options[:page], options[:lang],
-                                             options[:private], options[:org],
-                                             options[:org_type])
-        if valid_response?(results, "No repositories.")
-          show_results(@@output_formats, results.data, options, results.data['paging'])
+        results = Veye::API::Github.get_list(
+          api_key, options[:page], options[:lang],
+          options[:private], options[:org], options[:org_type]
+        )
+        if valid_response?(results, 'No repositories.')
+          paging = results.data['paging']
+          show_results(@output_formats, results.data, options, paging)
         end
       end
     end
   end
 end
-
