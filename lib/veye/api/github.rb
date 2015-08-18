4b0ed50dadd1fe561f7d289a99acfc1a634bdb80
Timo Sulg <timgluz@gmail.com>
2015-08-18 21:41:11 +0200

    reduce code smell in github api module;

--- lib/veye/api/github.rb
+++ lib/veye/api/github.rb
@@ -1,50 +1,50 @@
 module Veye
   module API
+    # Api wrappers for Github endpoint
     module Github
-      RESOURCE_PATH = "/github"
+      RESOURCE_PATH = '/github'
       def self.encode_repo_key(repo_key)
-        repo_key.to_s.gsub(/\//, ":").gsub(/\./, "~")
+        repo_key.to_s.gsub(/\//, ':').gsub(/\./, '~')
       end
-
 
       def self.get_list(api_key, page = 1, lang = nil, privat = nil, org = nil, type = nil)
         params = {
           api_key: api_key,
           page: page || 1
         }
-        params[:lang]     = lang.to_s.downcase if lang
+        params[:lang] = lang.to_s.downcase if lang
         unless private.nil?
-         params[:private]  = privat == 'true' || privat == 't' || privat == true
+          params[:private] = privat == 'true' || privat == 't' || privat == true
         end
         params[:org_name] = org if org
         params[:org_type] = type if type
 
         github_api = Resource.new(RESOURCE_PATH)
-        qparams = { :params => params }
+        qparams = { params: params }
         github_api.resource.get(qparams) do |response, request, result|
           JSONResponse.new(request, result, response)
         end
       end
 
       def self.import_all(api_key, force = false)
-        params = {api_key: api_key}
+        params = { api_key: api_key }
         params[:force] = force || false
-        qparams = {params: params}
-        github_api = Resource.new(RESOURCE_PATH)
+        qparams = { params: params }
+        github_api = Resource.new("#{RESOURCE_PATH}/sync")
 
-        github_api.resource['/sync'].get(qparams) do |response, request, result|
+        github_api.resource.get(qparams) do |response, request, result|
           JSONResponse.new(request, result, response)
         end
       end
 
       def self.get_repo(api_key, repo_name, branch = nil, file = nil)
-        safe_repo_name = self.encode_repo_key(repo_name)
+        safe_repo_name = encode_repo_key(repo_name)
         github_api = Resource.new("#{RESOURCE_PATH}/#{safe_repo_name}")
-        qparams = {api_key: api_key}
+        qparams = { api_key: api_key }
         qparams[:branch] = branch unless branch.nil?
         qparams[:file] = file unless file.nil?
 
-        github_api.resource.get({params: qparams}) do |response, request, result|
+        github_api.resource.get({ params: qparams }) do |response, request, result|
           JSONResponse.new(request, result, response)
         end
       end
@@ -53,7 +53,7 @@ module Veye
         safe_repo_name = encode_repo_key(repo_name)
         github_api = Resource.new("#{RESOURCE_PATH}/#{safe_repo_name}")
 
-        params = {api_key: api_key}
+        params = { api_key: api_key }
         params[:branch] = branch unless branch.nil?
         params[:file] = filename unless filename.nil?
 
@@ -63,8 +63,8 @@ module Veye
       end
 
       def self.delete_repo(api_key, repo_name, branch = nil)
-        safe_repo_key = self.encode_repo_key(repo_name)
-        qparams = { :api_key => api_key }
+        safe_repo_key = encode_repo_key(repo_name)
+        qparams = { api_key: api_key }
         qparams[:branch] = branch unless branch.nil?
         github_api = Resource.new("#{RESOURCE_PATH}/#{safe_repo_key}")
 
@@ -73,14 +73,15 @@ module Veye
         end
       end
 
+      # TODO: add tests
       def self.search(api_key, term, lang = nil, user = nil, page = 1)
         github_api = Resource.new("#{RESOURCE_PATH}/search")
 
-        params = {api_key: api_key, q: search_term}
+        params = { api_key: api_key, q: term }
         params[:langs] = lang unless lang.nil?
         params[:users] = user unless user.nil?
-        params[:page]  = page || "1"
-        qparams = {params: params}
+        params[:page]  = page || '1'
+        qparams = { params: params }
 
         github_api.resource.get(qparams) do |response, request, result|
           JSONResponse.new(request, result, response)
