1ccf9bd24bb9bdb89e029f18533e97c6eaa28c46
Timo Sulg <timgluz@gmail.com>
2015-08-18 20:47:08 +0200

    rubocopped github/sync class;

--- lib/veye/github/sync.rb
+++ lib/veye/github/sync.rb
@@ -2,20 +2,21 @@ require_relative '../base_executor.rb'
 
 module Veye
   module Github
+    # Sync class is responsible for updating info on VersionEye
     class Sync < BaseExecutor
       def self.import_all(api_key, options)
         response = Veye::API::Github.import_all(api_key, options[:force])
-        catch_request_error(response, "Can not import repositories from Github")
+        catch_request_error(response, 'Can not import repositories from Github')
         show_result(response)
       end
 
       def self.show_result(response)
-        unless response.data["changed"]
-          printf("%s - %s\n",
-                 "No changes.".color(:red),
-                 "Use `force` flag if you want to reload everything.")
-        else
+        if response.data['changed']
           printf "Imported. #{response.data['msg']}\n".color(:green)
+        else
+          printf("%s - %s\n",
+                 'No changes.'.color(:red),
+                 'Use `force` flag if you want to reload everything.')
         end
       end
     end
