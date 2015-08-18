7c9cbeabb5ab36adcaa741fcd07156eb36ca157a
Timo Sulg <timgluz@gmail.com>
2015-08-18 21:27:01 +0200

    reduced code smell in base_resource;

--- lib/veye/api/base_resource.rb
+++ lib/veye/api/base_resource.rb
@@ -1,28 +1,28 @@
 module Veye
   module API
-    class  BaseResource
+    # extends child classes with URL and API helpers
+    class BaseResource
       attr_reader :resource, :full_path
 
       def initialize(path = nil)
-        @full_path = "#{$global_options[:url].to_s}#{path.to_s}"
+        @full_path = "#{$global_options[:url]}#{path}"
       end
 
       def self.build_url(global_options)
         resource_url = nil
         protocol = global_options[:protocol]
         server = global_options[:server]
-        port = global_options.fetch(:port, "")
+        port = global_options.fetch(:port, '')
         path = global_options[:path]
 
-        unless port.to_s.empty?
-            resource_url = "#{protocol}://#{server}:#{port}/#{path}"
+        if port.to_s.empty?
+          resource_url = "#{protocol}://#{server}/#{path}"
         else
-            resource_url = "#{protocol}://#{server}/#{path}"
+          resource_url = "#{protocol}://#{server}:#{port}/#{path}"
         end
 
-        return resource_url
+        resource_url
       end
     end
-
   end
 end
