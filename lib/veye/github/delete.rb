36810163227cf247c2ab126eb6955bf319305bf1
Timo Sulg <timgluz@gmail.com>
2015-08-18 20:14:48 +0200

    removed codefart from github/delete class;

--- lib/veye/github/delete.rb
+++ lib/veye/github/delete.rb
@@ -2,22 +2,24 @@ require_relative '../base_executor.rb'
 
 module Veye
   module Github
+    # Delete class include methods to cleanup projects imported from Github
     class Delete < BaseExecutor
-
       def self.delete_repo(api_key, repo_name, options)
-        response = Veye::API::Github.delete_repo(api_key, repo_name, options[:branch])
+        response = Veye::API::Github.delete_repo(
+          api_key, repo_name, options[:branch]
+        )
         show_result(response)
       end
 
       def self.show_result(response)
-        unless response.success
-          printf("Cant delete - %s\n%s\n", response.message.color(:red),
-                                           response.data['error'])
-        else
+        if response.success
           printf "Deleted\n".color(:green)
+        else
+          printf("Cant delete - %s\n%s\n",
+                 response.message.color(:red),
+                 response.data['error'])
         end
       end
     end
   end
 end
-
