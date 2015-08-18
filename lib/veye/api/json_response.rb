8a1768ebc4de70ca4f998aed3a59dbc71b416f86
Timo Sulg <timgluz@gmail.com>
2015-08-18 21:49:12 +0200

    reduce code smell in json_response;

--- lib/veye/api/json_response.rb
+++ lib/veye/api/json_response.rb
@@ -2,55 +2,51 @@ require 'json'
 
 module Veye
   module API
+    # Unified API response class
     class JSONResponse
       attr_reader :code, :success, :message, :data, :url, :headers
       def initialize(request, result, response)
-         @url = request.url
-         @headers = request.headers
-         @code = result.code.to_i
-
-         response_data = JSON.parse(response)
-
-         @success, @message = success?(result, response_data)
-         @data = response_data
+        @url = request.url
+        @headers = request.headers
+        @code = result.code.to_i
+        response_data = JSON.parse(response)
+        @success, @message = success?(result)
+        @data = response_data
       end
 
-      def success?(result, response_data)
+      def success?(result)
         @code = result.code.to_i
         success = false
-         
-        case @code 
+        case @code
         when 200
           success = true
-          message = "fetched successfully"
+          message = 'fetched successfully'
         when 201
           success = true
-          message = "created successfully"
+          message = 'created successfully'
         when 400
-          message = "bad request - wrong parameters, data"
+          message = 'bad request - wrong parameters, data'
         when 401
-          message = "not authorized - add apikey or update settings file ~/.veye.rc"
+          message = 'not authorized - add apikey or update settings.'
         when 403
-          message = "forbidden - server refused execute query"
+          message = 'forbidden - server refused execute query'
         when 413
-          message = "request entity too big - use smaller data object"
+          message = 'request entity too big - use smaller data object'
         when 500
-          message = "internal server error - write to us"
+          message = 'internal server error - write to us'
         when 501
-          message = "not implemented - write to us"
+          message = 'not implemented - write to us'
         when 503
-          message = "service unavailable - temporary overloaded - write to us."
+          message = 'service unavailable - temporary overloaded - write to us'
         when 531
-          message = "not authorized - add apikey or update settings file ~/.veye.rc"
+          message = 'not authorized - add or update api key'
         else
           success = false
-          message = ""
+          message = ''
         end
 
-         return success, message
+        [success, message]
       end
-
-
     end
   end
 end
