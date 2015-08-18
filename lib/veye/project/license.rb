ad70a22ff1947fe0559c21c131115ec4a9775314
Timo Sulg <timgluz@gmail.com>
2015-08-18 19:55:25 +0200

    removed codefart from project license class;

--- lib/veye/project/license.rb
+++ lib/veye/project/license.rb
@@ -3,19 +3,20 @@ require_relative '../base_executor.rb'
 
 module Veye
   module Project
+    # Licence class holds commands for checking licenses in the project.
     class License < BaseExecutor
-      @@output_formats = {
-        "csv"     => Project::LicenceCSV.new,
-        "json"    => Project::LicenceJSON.new,
-        "pretty"  => Project::LicencePretty.new,
-        "table"   => Project::LicenceTable.new
+      @output_formats = {
+        'csv'     => Project::LicenceCSV.new,
+        'json'    => Project::LicenceJSON.new,
+        'pretty'  => Project::LicencePretty.new,
+        'table'   => Project::LicenceTable.new
       }
 
       def self.get_licenses(project_key, api_key, options)
         results = Veye::API::Project.get_licenses(api_key, project_key)
         err_msg = "Cant access a information for project `#{project_key}`."
         catch_request_error(results, err_msg)
-        show_results(@@output_formats, results.data, options)
+        show_results(@output_formats, results.data, options)
       end
     end
   end
