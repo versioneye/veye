af067a467e0c27e4d96745ff27a946af2890ab75
Timo Sulg <timgluz@gmail.com>
2015-08-19 16:12:14 +0200

    move global_options initialization out of module;

--- lib/api.rb
+++ lib/api.rb
@@ -1,13 +1,15 @@
 require 'veye/api'
 
+# -- initialize global options
+$global_options = {
+  protocol: 'https',
+  server: 'www.versioneye.com',
+  path: 'api/v2/',
+  port: nil
+}
+$global_options[:url] = Veye::API::Resource.build_url($global_options)
+
 module Veye
   module API
-    $global_options = {
-      protocol: 'https',
-      server: 'www.versioneye.com',
-      path: 'api/v2/',
-      port: nil
-    }
-    $global_options[:url] = Resource.build_url($global_options)
   end
 end
